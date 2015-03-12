a49e34e24d9b9d662d4c62f2b4fdd5b4dc3ccd00
Ilya Lyubimov <villytiger@gmail.com>
2015-03-12 22:37:09 +0300

    Add sign in, sign up calls to wamp server

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,1 +1,28 @@
-var app = angular.module('cottonway', ["cottonwayControllers"]);
+var app = angular.module('cottonway', [
+    'vxWamp',
+    "cottonwayControllers"]);
+
+app.config(function($wampProvider) {
+    $wampProvider.init({
+        url: 'wss://cottonway.club/ws/',
+        realm: 'realm1'
+    });
+});
+
+app.run(function($wamp){
+    $wamp.open();
+});
+
+app.factory('errors',function(){
+    var errorDesc = {
+        1: 'Ошибка'
+    };
+    
+    return {
+        check: function(r) {
+            if (r.callStatus !== 0) {
+                throw errorDesc[r.callStatus];
+            }
+        }
+    };
+});
--- frontend/js/controllers.js
+++ frontend/js/controllers.js
@@ -1,4 +1,25 @@
 var cottonwayControllers = angular.module('cottonwayControllers', []);
 
-cottonwayControllers.controller("SignInCtrl", function($scope) {
+cottonwayControllers.controller('SignInCtrl', function($scope, $wamp, errors) {
+    $scope.isConnected = false;
+    
+    $scope.$on("$wamp.open", function () {
+        $scope.isConnected = true;
+    });
+
+    $scope.$on("$wamp.close", function () {
+        $scope.isConnected = false;
+    });
+
+    $scope.signUp = function(email, username, password) {
+        $wamp.call('club.cottonway.auth.sign_up', [email, username, password], {}, {disclose_me: true}).then(function(r) {
+            errors.check(r);
+        });
+    };
+    
+    $scope.signIn = function(username, password) {
+        $wamp.call('club.cottonway.auth.sign_in', [username, password], {}, {disclose_me: true}).then(function(r) {
+            errors.check(r);
+        });
+    };
 });
