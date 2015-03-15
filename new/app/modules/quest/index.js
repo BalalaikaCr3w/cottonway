e1b9601054279466f1a6adcbfe3567cb2204feb1
azrael <azrael.25.91@gmail.com>
2015-03-15 21:54:26 +0300

    quest api

--- new/app/modules/quest/index.js
+++ new/app/modules/quest/index.js
@@ -6,26 +6,14 @@ controllers.controller('questController', ['$scope', 'apiService', questControll
 
 function questController ($scope, apiService) {
 
-    $scope.messages = [{
-        time: new Date().getTime(),
-        text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.'
-    }, {
-        time: new Date().getTime() - 2*60*1000,
-        text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet.'
-    }, {
-        time: new Date().getTime() - 5*60*1000,
-        text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.'
-    }, {
-        time: new Date().getTime() - 10*60*1000,
-        text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.'
-    }];
-
-    _.each($scope.messages, function (item) {
-        item.timeFormatted = moment(item.time).format('DD MMMM, HH:mm');
-    });
-
     apiService.call('club.cottonway.quest.steps')
         .then(function (response) {
-            console.log(response);
+
+            $scope.messages = _.map(response.steps, function (item) {
+
+                return _.extend({
+                    timeFormatted: moment(item.time).format('DD MMMM, HH:mm')
+                }, item);
+            });
         });
 }
