3b28d2ed1ab75249d249fbcf07ccf361fffac7f9
azrael <azrael.25.91@gmail.com>
2015-03-17 22:42:05 +0300

    admin page update, refactor

--- frontend/app/modules/admin/index.js
+++ frontend/app/modules/admin/index.js
@@ -1,7 +1,32 @@
-var controllers = require('../../core/controllers');
+var controllers = require('../../core/controllers'),
+    _ = require('lodash');
 
-controllers.controller('adminController', ['$scope', 'apiService', adminController]);
+controllers.controller('adminController', ['$scope', '$timeout', 'apiService', adminController]);
 
-function adminController ($scope, apiService) {
+function adminController ($scope, $timeout, apiService) {
 
+    $timeout(function () {
+        apiService.call('club.cottonway.quest.all_steps')
+            .then(function (response) {
+                $scope.steps = response.steps;
+            });
+
+        apiService.call('club.cottonway.common.peers')
+            .then(function (response) {
+                $scope.peers = response.peers;
+                console.log(response);
+            });
+    }, 1000);
+
+    $scope.openStepToPeer = function () {
+
+        //club.cottonway.quest.open_step(peerId, stepId) -> void
+    };
+
+    $scope.formatPeer = function (id) {
+
+        $scope.peer = _.find($scope.peers, {id: id});
+
+        return $scope.peer && $scope.peer.name || '';
+    };
 }
